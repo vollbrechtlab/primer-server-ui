1f04229d5f154948389c3e76c434e4029eaf40ba
Takao Shibamoto <takaos@iastate.edu>
2017-07-31 20:29:43 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
 function pickPrimers(){
-	var data = readAndFOrmatData();
+	var data = readAndFormatData();
 }
 
+// read data from the form and format it
 function readAndFormatData(){
 	var inputs = document.getElementsByTagName('input');
+	var formattedData = {};
 
-	for (var i = 0; i < inputs.length; ++i) {
-	    console.log(inputs[i].type);
+	for (var i = 0; i < inputs.length; ++i) 
+	{
+	    if(inputs[i].type == 'checkboxa'){
+	    	formattedData[inputs[i].name] = inputs[i].checked;
+	    }
+	    if(inputs[i].type == 'text'){
+	    	if(isNumeric(inputs[i].value)){
+	    		formattedData[inputs[i].name] = +inputs[i].value;
+	    	} else {
+	    		console.log(inputs[i].value);
+	    		formattedData[inputs[i].name] = JSON.parse("[" + inputs[i].value + "]");
+	    		console.log(formattedData[inputs[i].name]);
+	    	}
+	    }
 	}
 
-	textareas = document.getElementsByTagName('textarea');
+	var textareas = document.getElementsByTagName('textarea');
 
 	for (var i = 0; i < textareas.length; ++i) {
 	    console.log(textareas[i]);
 	}
+
+	console.log(formattedData);
+}
+
+function isNumeric(num){
+    return !isNaN(num)
 }
