fcdf5fc70f9f6231cee8176b714e052e00462229
Takao Shibamoto <takaos@iastate.edu>
2017-08-02 22:42:12 -0500

    complete new version

--- js/main.js
+++ js/main.js
@@ -1,37 +1,11 @@
-function pickPrimers(){
-	var data = readAndFormatData();
-}
+$(".pick_left_primer_check").click(pick_toggle);
+$(".pick_internal_oligo_check").click(pick_toggle);
+$(".pick_right_primer_check").click(pick_toggle);
 
-// read data from the form and format it
-function readAndFormatData(){
-	var inputs = document.getElementsByTagName('input');
-	var formattedData = {};
-
-	for (var i = 0; i < inputs.length; ++i) 
-	{
-	    if(inputs[i].type == 'checkboxa'){
-	    	formattedData[inputs[i].name] = inputs[i].checked;
-	    }
-	    if(inputs[i].type == 'text'){
-	    	if(isNumeric(inputs[i].value)){
-	    		formattedData[inputs[i].name] = +inputs[i].value;
-	    	} else {
-	    		console.log(inputs[i].value);
-	    		formattedData[inputs[i].name] = JSON.parse("[" + inputs[i].value + "]");
-	    		console.log(formattedData[inputs[i].name]);
-	    	}
-	    }
-	}
-
-	var textareas = document.getElementsByTagName('textarea');
-
-	for (var i = 0; i < textareas.length; ++i) {
-	    console.log(textareas[i]);
-	}
-
-	console.log(formattedData);
-}
-
-function isNumeric(num){
-    return !isNaN(num)
+function pick_toggle() {
+    if($(this).is(":checked")) {
+        $(".answer_" + this.className).hide("slow");
+    } else {
+        $(".answer_" + this.className).show("slow");
+    }
 }
