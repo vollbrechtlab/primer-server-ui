525a41fa016e8d948fdecb5736f0527957784997
Takao Shibamoto <takaos@iastate.edu>
2017-08-09 22:24:50 -0500

    improved additional param prototype

--- src/app/additional-param/additional-param.component.ts
+++ src/app/additional-param/additional-param.component.ts
@@ -15,7 +15,7 @@ import 'rxjs/add/observable/fromEvent';
   styleUrls: ['./additional-param.component.css']
 })
 export class AdditionalParamComponent {
-  displayedColumns = ['userName', 'progress'];
+  displayedColumns = ['view_name', 'name', 'value'];
   exampleDatabase = new ExampleDatabase();
   dataSource: ExampleDataSource | null;
 
@@ -183,9 +183,161 @@ const NAMES = [
   'PRIMER_LOWERCASE_MASKING', 
   'PRIMER_PAIR_WT_PRODUCT_SIZE_GT'];
 
+const VIEW_NAMES = [
+  'SEQUENCE_EXCLUDED_REGION', 
+  'SEQUENCE_INCLUDED_REGION',
+  'SEQUENCE_FORCE_LEFT_END', 
+  'SEQUENCE_QUALITY', 
+  'SEQUENCE_FORCE_LEFT_START', 
+  'SEQUENCE_START_CODON_POSITION', 
+  'SEQUENCE_FORCE_RIGHT_END', 
+  'SEQUENCE_OVERLAP_JUNCTION_LIST', 
+  'SEQUENCE_FORCE_RIGHT_START', 
+  'SEQUENCE_ID', 
+  'SEQUENCE_PRIMER_PAIR_OK_REGION_LIST', 
+  'PRIMER_DNA_CONC', 
+  'PRIMER_MAX_END_GC', 
+  'PRIMER_PAIR_WT_PRODUCT_SIZE_LT', 
+  'PRIMER_DNTP_CONC', 
+  'PRIMER_MAX_END_STABILITY', 
+  'PRIMER_PAIR_WT_PRODUCT_TM_GT', 
+  'PRIMER_EXPLAIN_FLAG', 
+  'PRIMER_MAX_GC', 
+  'PRIMER_PAIR_WT_PRODUCT_TM_LT', 
+  'PRIMER_FIRST_BASE_INDEX', 
+  'PRIMER_MAX_HAIRPIN_TH', 
+  'PRIMER_PAIR_WT_PR_PENALTY', 
+  'PRIMER_GC_CLAMP', 
+  'PRIMER_MAX_LIBRARY_MISPRIMING', 
+  'PRIMER_PAIR_WT_TEMPLATE_MISPRIMING', 
+  'PRIMER_INSIDE_PENALTY', 
+  'PRIMER_MAX_NS_ACCEPTED', 
+  'PRIMER_PAIR_WT_TEMPLATE_MISPRIMING_TH', 
+  'PRIMER_INTERNAL_DNA_CONC', 
+  'PRIMER_MAX_POLY_X', 
+  'PRIMER_PICK_ANYWAY', 
+  'PRIMER_INTERNAL_DNTP_CONC', 
+  'PRIMER_MAX_SELF_ANY', 
+  'PRIMER_INTERNAL_MAX_GC', 
+  'PRIMER_MAX_SELF_ANY_TH', 
+  'PRIMER_INTERNAL_MAX_HAIRPIN_TH', 
+  'PRIMER_MAX_SELF_END', 
+  'PRIMER_INTERNAL_MAX_LIBRARY_MISHYB', 
+  'PRIMER_MAX_SELF_END_TH', 
+  'PRIMER_PRODUCT_MAX_TM', 
+  'PRIMER_INTERNAL_MAX_NS_ACCEPTED', 
+  'PRIMER_MAX_SIZE', 
+  'PRIMER_PRODUCT_MIN_TM', 
+  'PRIMER_INTERNAL_MAX_POLY_X', 
+  'PRIMER_MAX_TEMPLATE_MISPRIMING', 
+  'PRIMER_PRODUCT_OPT_SIZE', 
+  'PRIMER_INTERNAL_MAX_SELF_ANY', 
+  'PRIMER_MAX_TEMPLATE_MISPRIMING_TH', 
+  'PRIMER_PRODUCT_OPT_TM', 
+  'PRIMER_INTERNAL_MAX_SELF_ANY_TH', 
+  'PRIMER_INTERNAL_MAX_SELF_END', 
+  'PRIMER_MIN_3_PRIME_OVERLAP_OF_JUNCTION', 
+  'PRIMER_QUALITY_RANGE_MAX', 
+  'PRIMER_INTERNAL_MAX_SELF_END_TH', 
+  'PRIMER_MIN_5_PRIME_OVERLAP_OF_JUNCTION', 
+  'PRIMER_QUALITY_RANGE_MIN', 
+  'PRIMER_INTERNAL_MAX_SIZE', 
+  'PRIMER_MIN_END_QUALITY', 
+  'PRIMER_INTERNAL_MAX_TM', 
+  'PRIMER_MIN_GC', 
+  'PRIMER_SALT_DIVALENT', 
+  'PRIMER_INTERNAL_MIN_GC', 
+  'PRIMER_MIN_LEFT_THREE_PRIME_DISTANCE', 
+  'PRIMER_SALT_MONOVALENT', 
+  'PRIMER_INTERNAL_MIN_QUALITY', 
+  'PRIMER_MIN_QUALITY', 
+  'PRIMER_SEQUENCING_ACCURACY', 
+  'PRIMER_INTERNAL_MIN_SIZE', 
+  'PRIMER_MIN_RIGHT_THREE_PRIME_DISTANCE', 
+  'PRIMER_SEQUENCING_INTERVAL', 
+  'PRIMER_INTERNAL_MIN_TM', 
+  'PRIMER_MIN_SIZE', 
+  'PRIMER_SEQUENCING_LEAD', 
+  'PRIMER_INTERNAL_MISHYB_LIBRARY', 
+  'PRIMER_MIN_THREE_PRIME_DISTANCE', 
+  'PRIMER_SEQUENCING_SPACING', 
+  'PRIMER_INTERNAL_MUST_MATCH_FIVE_PRIME', 
+  'PRIMER_TASK', 
+  'PRIMER_INTERNAL_MUST_MATCH_THREE_PRIME', 
+  'PRIMER_MISPRIMING_LIBRARY', 
+  'PRIMER_THERMODYNAMIC_OLIGO_ALIGNMENT', 
+  'PRIMER_INTERNAL_OPT_GC_PERCENT', 
+  'PRIMER_MUST_MATCH_FIVE_PRIME', 
+  'PRIMER_THERMODYNAMIC_PARAMETERS_PATH', 
+  'PRIMER_INTERNAL_OPT_SIZE', 
+  'PRIMER_MUST_MATCH_THREE_PRIME', 
+  'PRIMER_THERMODYNAMIC_TEMPLATE_ALIGNMENT', 
+  'PRIMER_INTERNAL_OPT_TM', 
+  'PRIMER_NUM_RETURN', 
+  'PRIMER_INTERNAL_SALT_DIVALENT', 
+  'PRIMER_OPT_GC_PERCENT', 
+  'PRIMER_WT_END_QUAL', 
+  'PRIMER_INTERNAL_SALT_MONOVALENT', 
+  'PRIMER_OPT_SIZE', 
+  'PRIMER_WT_END_STABILITY', 
+  'PRIMER_INTERNAL_WT_END_QUAL', 
+  'PRIMER_WT_GC_PERCENT_GT', 
+  'PRIMER_INTERNAL_WT_GC_PERCENT_GT', 
+  'PRIMER_OUTSIDE_PENALTY', 
+  'PRIMER_WT_GC_PERCENT_LT', 
+  'PRIMER_INTERNAL_WT_GC_PERCENT_LT', 
+  'PRIMER_PAIR_MAX_COMPL_ANY', 
+  'PRIMER_WT_HAIRPIN_TH', 
+  'PRIMER_INTERNAL_WT_HAIRPIN_TH', 
+  'PRIMER_PAIR_MAX_COMPL_ANY_TH', 
+  'PRIMER_WT_LIBRARY_MISPRIMING', 
+  'PRIMER_INTERNAL_WT_LIBRARY_MISHYB', 
+  'PRIMER_PAIR_MAX_COMPL_END', 
+  'PRIMER_WT_NUM_NS', 
+  'PRIMER_INTERNAL_WT_NUM_NS', 
+  'PRIMER_PAIR_MAX_COMPL_END_TH', 
+  'PRIMER_WT_POS_PENALTY', 
+  'PRIMER_INTERNAL_WT_SELF_ANY', 
+  'PRIMER_WT_SELF_ANY', 
+  'PRIMER_INTERNAL_WT_SELF_ANY_TH', 
+  'PRIMER_PAIR_MAX_LIBRARY_MISPRIMING', 
+  'PRIMER_WT_SELF_ANY_TH', 
+  'PRIMER_INTERNAL_WT_SELF_END', 
+  'PRIMER_PAIR_MAX_TEMPLATE_MISPRIMING', 
+  'PRIMER_WT_SELF_END', 
+  'PRIMER_INTERNAL_WT_SELF_END_TH', 
+  'PRIMER_PAIR_MAX_TEMPLATE_MISPRIMING_TH', 
+  'PRIMER_WT_SELF_END_TH', 
+  'PRIMER_INTERNAL_WT_SEQ_QUAL', 
+  'PRIMER_PAIR_WT_COMPL_ANY', 
+  'PRIMER_WT_SEQ_QUAL', 
+  'PRIMER_INTERNAL_WT_SIZE_GT', 
+  'PRIMER_PAIR_WT_COMPL_ANY_TH', 
+  'PRIMER_WT_SIZE_GT', 
+  'PRIMER_INTERNAL_WT_SIZE_LT', 
+  'PRIMER_PAIR_WT_COMPL_END', 
+  'PRIMER_WT_SIZE_LT', 
+  'PRIMER_INTERNAL_WT_TM_GT', 
+  'PRIMER_PAIR_WT_COMPL_END_TH', 
+  'PRIMER_WT_TEMPLATE_MISPRIMING', 
+  'PRIMER_INTERNAL_WT_TM_LT', 
+  'PRIMER_PAIR_WT_DIFF_TM', 
+  'PRIMER_WT_TEMPLATE_MISPRIMING_TH', 
+  'PRIMER_LIBERAL_BASE', 
+  'PRIMER_PAIR_WT_IO_PENALTY', 
+  'PRIMER_WT_TM_GT', 
+  'PRIMER_LIB_AMBIGUITY_CODES_CONSENSUS', 
+  'PRIMER_PAIR_WT_LIBRARY_MISPRIMING', 
+  'PRIMER_WT_TM_LT', 
+  'PRIMER_LOWERCASE_MASKING', 
+  'PRIMER_PAIR_WT_PRODUCT_SIZE_GT'
+];
+
 export interface UserData {
+
   name: string;
-  progress: string;
+  view_name: string;
+  value: string;
 }
 
 
@@ -197,27 +349,19 @@ export class ExampleDatabase {
 
   constructor() {
     // Fill up the database with 100 users.
-    for (let i = 0; i < 100; i++) { this.addUser(); }
+    for (let i = 0; i < NAMES.length; i++) { this.addUser(i); }
   }
 
   /** Adds a new user to the database. */
-  addUser() {
+  addUser(i) {
     const copiedData = this.data.slice();
-    copiedData.push(this.createNewUser());
+    var newSection = {
+      view_name: VIEW_NAMES[i],
+      name: NAMES[i],
+      value: ""
+    }
+    copiedData.push(newSection);
     this.dataChange.next(copiedData);
-  }
-
-  /** Builds and returns a new User. */
-  private createNewUser() {
-    const name =
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-    return {
-      id: (this.data.length + 1).toString(),
-      name: name,
-      progress: Math.round(Math.random() * 100).toString(),
-    };
   }
 }
 
@@ -246,7 +390,7 @@ export class ExampleDataSource extends DataSource<any> {
 
     return Observable.merge(...displayDataChanges).map(() => {
       return this._exampleDatabase.data.slice().filter((item: UserData) => {
-        let searchStr = (item.name).toLowerCase();
+        let searchStr = (item.name+item.view_name).toLowerCase();
         return searchStr.indexOf(this.filter.toLowerCase()) != -1;
       });
     });
