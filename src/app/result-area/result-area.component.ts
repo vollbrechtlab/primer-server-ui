fee455fa05a98f667508a93321ca57fc48f8b61e
Takao Shibamoto <takaos@iastate.edu>
2017-08-09 22:03:34 -0500

    added additional param prototype

--- src/app/result-area/result-area.component.ts
+++ src/app/result-area/result-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { P3DataSharingService } from '../p3-data-sharing.service';
 
@@ -10,17 +11,20 @@ import { P3DataSharingService } from '../p3-data-sharing.service';
 export class ResultAreaComponent implements OnInit {
 
   p3Result: any;
+  subscription: Subscription;
 
   constructor(
     private p3DataSharingSerice: P3DataSharingService
-  ) { }
+  ) { 
+    this.subscription = this.p3DataSharingSerice.getP3Result()
+      .subscribe(result => { 
+        this.p3Result = result; 
+    });
+  }
 
   ngOnInit() {
-    console.log(this.p3DataSharingSerice.p3Result);
+    
   }
 
-  updateResult(){
-    console.log('yey!');
-  }
 
 }
