ace6a820b52ff4a8b67efba2d2db8068a3cd090b
Takao Shibamoto <takaos@iastate.edu>
2017-08-11 17:08:37 -0500

    improved additional param

--- src/app/additional-param/additional-param.component.ts
+++ src/app/additional-param/additional-param.component.ts
@@ -1,13 +1,10 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
-import {DataSource} from '@angular/cdk';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Observable} from 'rxjs/Observable';
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
 import 'rxjs/add/operator/startWith';
-import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/observable/fromEvent';
+
+import {P3Params} from '../p3-params';
 
 @Component({
   selector: 'app-additional-param',
@@ -16,6 +13,32 @@ import 'rxjs/add/observable/fromEvent';
 })
 export class AdditionalParamComponent {
 
-  ngOnInit() {
+  paramCtrl: FormControl;
+  filteredParamIds: any;
+
+  params: P3Params;
+  paramId: number;
+
+  constructor() {
+    this.params = new P3Params();
+
+    this.paramCtrl = new FormControl();
+    // Observable will be returned
+    this.filteredParamIds = this.paramCtrl.valueChanges
+        .startWith(null)
+        .map(ids => this.filterParamIds(ids));
+  }
+
+  filterParamIds(val: string) {
+    if(val){
+      return this.params.searchData(val);
+    } else {
+      return this.params.ids; 
+    }
+  }
+
+  paramSelected(id){
+    this.paramId = id;
+    console.log('selected ' + id);
   }
 }
