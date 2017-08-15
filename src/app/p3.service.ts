a7652b9858ac33d537ec1ed75711d101fab516de
Takao Shibamoto <takaos@iastate.edu>
2017-08-15 01:10:46 -0500

    Moved param data to p3-params.ts

--- src/app/p3.service.ts
+++ src/app/p3.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {P3Params} from './p3-params';
 
 @Injectable()
 export class P3Service {
-  params = [];
+  params = {};
 
-  constructor(private http: Http) {}
+  constructor() {
+    let p3ParamsObject = new P3Params();
+    this.params = p3ParamsObject.data;
+  }
 
-  loadParams() {
-    return this.http.get('assets/p3-params.json')
-                .toPromise()
-                .then(res => { this.params = res.json().data; });
+  convertRangelistToArr(rangelist: string){
+    var arr = [];
+    for(let i = 0; i < rangelist.length; i++){
+      
+    }
+    arr.push([]);
+    return arr;
+  }
+
+  convertArrToRangelist(arr: Array<Array<number>>){
+    return null;
   }
 }
