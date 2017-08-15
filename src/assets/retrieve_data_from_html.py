1eedb569157dc9d5fbd68ef728d3c5314bfa10fc
Takao Shibamoto <takaos@iastate.edu>
2017-08-14 23:29:47 -0500

    complete set of parameters

--- src/assets/retrieve_data_from_html.py
+++ src/assets/retrieve_data_from_html.py
@@ -36,41 +36,59 @@ def check_basic_param(param_name):
 	return False
 
 params = []
-params.append(["name","description","type","default_value","setting_type"])
+params.append(["name","description","type","default_value","setting_type", "form_type"])
 for tag in soup.find_all("h3"):
 	atag = tag.find("a")
 	try:
 		if ";" in atag.text and not "P3_" in atag.text:
+			# get the type and default value string
 			param_type_default_val = atag.text.split('(')[1]
 			param_type_default_val = param_type_default_val.replace(')', '')
 			param_type_default_val = param_type_default_val.replace('default', '')
-			#param_type_default_val = param_type_default_val.replace(' ', '')
 			param_type_default_val = param_type_default_val.split(';')
 
+			# get the parameter type
 			param_type = param_type_default_val[0]
+
+			# get the default value
 			param_default_val = param_type_default_val[1]
 
+			# fix some values
 			if "decimal" in param_type:
 				param_type = "decimal"
-
+			if "float" in param_type:
+				param_type = "float"
 			if "empty" in param_default_val:
 				param_default_val = ""
 
+			# get the parameter name
 			param_name = str(atag["name"])
 
+			# define the form type
+			form_type = "input_text"
+			if param_name == "SEQUENCE_TEMPLATE":
+				form_type = "textarea"
+			elif param_type == "int" or param_type == "float" or param_type == "decimal":
+				form_type = "input_number"
+			elif param_type == "boolean":
+				form_type = "checkbox"
+
+			# get the parameter description
 			param_description = ""
 			param_description_tag = tag.find_next_sibling()
 			while(param_description_tag.name != "h3"):
 				param_description += param_description_tag.text
 				param_description_tag = param_description_tag.find_next_sibling()
 
+			# define the setting type
+			setting_type = "additional"
 			if check_basic_param(param_name):
-				params.append([param_name, param_description, param_type, param_default_val, "basic"])
-			else:
-				params.append([param_name, param_description, param_type, param_default_val, "additional"])
+				setting_type = "basic"
+
+			params.append([param_name, param_description, param_type, param_default_val, setting_type, form_type])
 	except:
 		pass
 
-with open("param_info_org.csv", 'w') as dest_csv:
+with open("param_info.csv", 'w') as dest_csv:
 	wr = csv.writer(dest_csv)
 	wr.writerows(params)
