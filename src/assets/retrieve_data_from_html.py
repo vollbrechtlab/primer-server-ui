746686fbcfa19b28e08001b6cd18f522763cdf3e
Takao Shibamoto <takaos@iastate.edu>
2017-08-14 22:59:58 -0500

    small fix

--- src/assets/retrieve_data_from_html.py
+++ src/assets/retrieve_data_from_html.py
@@ -1,3 +1,8 @@
+"""
+Retrieve the parameter data from the manual html file
+and save it as a csv file
+"""
+
 from bs4 import BeautifulSoup
 import csv, json
 
@@ -6,47 +11,36 @@ source_html = source_html_file.read()
 source_html_file.close()
 soup = BeautifulSoup(source_html, "html.parser")
 
-"""
-param_names = ""
-for tag in soup.find_all("h3"):
-	atag = tag.find("a")
-	try:
-		param_names += str(atag["name"]) + "\n"
-	except:
-		pass
-"""
-
-"""
-param_desciprions = []
-for tag in soup.find_all("p"):
-	param_desciprions.append(str(tag))
-print(param_desciprions[30])
-"""
-
-#destination_txt = open("param_info.txt","w")
-#destination_txt.write(param_names)
-#destination_txt.close()
-#source_html.close()
-
-"""
-tag = soup.find("h3")
-print(tag)
-tag = tag.find_next_sibling()
-print(tag)
-tag = tag.find_next_sibling()
-print(tag)
-tag = tag.find_next_sibling()
-print(tag)
-tag = tag.find_next_sibling()
-print(tag)
-"""
+basic_params = [
+	'SEQUENCE_TEMPLATE',
+	'PRIMER_PICK_LEFT_PRIMER',
+	'SEQUENCE_PRIMER_INPUT',
+	'PRIMER_PICK_INTERNAL_OLIGO',
+	'SEQUENCE_INTERNAL_OLIGO',
+	'PRIMER_PICK_RIGHT_PRIMER',
+	'SEQUENCE_PRIMER_REVCOMP',
+	'PRIMER_PRODUCT_SIZE_RANGE',
+	'SEQUENCE_TARGET',
+	'SEQUENCE_EXCLUDED_REGION',
+	'PRIMER_MIN_TM',
+	'PRIMER_OPT_TM',
+	'PRIMER_MAX_TM',
+	'PRIMER_PAIR_MAX_DIFF_TM',
+	'PRIMER_SALT_CORRECTIONS',
+	'PRIMER_TM_FORMULA'
+]
+def check_basic_param(param_name):
+	for p in basic_params:
+		if p in param_name:
+			return True
+	return False
 
 params = []
-params.append(["param_name","description","type","default_value"])
+params.append(["name","description","type","default_value","setting_type"])
 for tag in soup.find_all("h3"):
 	atag = tag.find("a")
 	try:
-		if(";" in atag.text):
+		if ";" in atag.text and not "P3_" in atag.text:
 			param_type_default_val = atag.text.split('(')[1]
 			param_type_default_val = param_type_default_val.replace(')', '')
 			param_type_default_val = param_type_default_val.replace('default', '')
@@ -55,6 +49,9 @@ for tag in soup.find_all("h3"):
 
 			param_type = param_type_default_val[0]
 			param_default_val = param_type_default_val[1]
+
+			if "decimal" in param_type:
+				param_type = "decimal"
 
 			if "empty" in param_default_val:
 				param_default_val = ""
@@ -67,22 +64,13 @@ for tag in soup.find_all("h3"):
 				param_description += param_description_tag.text
 				param_description_tag = param_description_tag.find_next_sibling()
 
-			params.append([param_name, param_description, param_type, param_default_val])
+			if check_basic_param(param_name):
+				params.append([param_name, param_description, param_type, param_default_val, "basic"])
+			else:
+				params.append([param_name, param_description, param_type, param_default_val, "additional"])
 	except:
 		pass
-#print(params[2])
 
-#params_string = "param_name,description,type,default_value\n"
-#for p in params:
-#	params_string += p[0] + "," + p[1] + "," + p[2] + "," + p[3] + "\n"
-
-#destination_txt = open("param_info.csv","w")
-#destination_txt.write(params_string)
-#destination_txt.close()
-
-#json_string = json.dumps(params)
-#print(json_string)
-
-with open("param_info.csv", 'w') as dest_csv:
+with open("param_info_org.csv", 'w') as dest_csv:
 	wr = csv.writer(dest_csv)
 	wr.writerows(params)
