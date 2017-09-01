0a0bc4b5f66aaaf5403d791ccb7990e2975e4418
Takao Shibamoto <takaos@iastate.edu>
2017-08-31 19:59:40 -0500

    trying to implement update function of the chart

--- src/app/sq-bar-chart/sq-bar-chart.component.ts
+++ src/app/sq-bar-chart/sq-bar-chart.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, SimpleChanges } from '@angular/core';
+import {BaseChartDirective} from 'ng2-charts/ng2-charts';
  
 @Component({
   selector: 'app-sq-bar-chart',
   templateUrl: './sq-bar-chart.component.html',
   styleUrls: ['./sq-bar-chart.component.css']
 })
-export class SqBarChartComponent {
+export class SqBarChartComponent implements OnInit {
+  @ViewChild(BaseChartDirective)
+  public chart: BaseChartDirective;
+
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
     responsive: true,
@@ -18,49 +22,72 @@ export class SqBarChartComponent {
       }]
     }
   };
-  public barChartLabels:string[] = ['Sequence1', "Sequence2"];
+  public barChartLabels:string[] = ['Sequence'];
   public barChartType:string = 'horizontalBar';
   public barChartLegend:boolean = true;
  
-  public barChartData:any[] = [
-    {data: [65], label: 'Excluded'},
-    {data: [28], label: 'Target'},
-    {data: [28], label: 'Normal'},
-    {data: [28], label: 'Excluded'}
-  ];
+  public barChartData:any[] = [];
 
-  public chartColors: any[] = [
-    {backgroundColor:"red"},
-    {backgroundColor:"grey"}
-  ];
+  public chartColors: any[] = [];
+
+  ngOnInit() {
+    this.barChartData = [
+      {data: [65], label: 'Excluded'},
+      {data: [28], label: 'Target'},
+      {data: [50], label: 'Normal'},
+      //{data: [17], label: 'Excluded'}
+    ];
+
+    this.chartColors = [
+      {backgroundColor:"red"},
+      {backgroundColor:"grey"},
+      {backgroundColor:"green"},
+      {backgroundColor:"red"}
+    ];
+
+    
+  }
  
   // events
   public chartClicked(e:any):void {
     console.log(e);
+    this.createSqChartData();
   }
  
   public chartHovered(e:any):void {
     console.log(e);
   }
  
-  public randomize():void {
-    // Only Change 3 values
-    let data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40];
-    let clone = JSON.parse(JSON.stringify(this.barChartData));
-    clone[0].data = data;
-    this.barChartData = clone;
-    /**
-     * (My guess), for Angular to recognize the change in the dataset
-     * it has to change the dataset variable directly,
-     * so one way around it, is to clone the data, change it and then
-     * assign it;
-     */
+  /*
+   * Create the sequence chart data
+   */
+  public createSqChartData():void {
+    var sequence_template = "AAAAAAAAAAAGGGGGGGGGCCCCAAAA";
+    var targets = [[0, 3],[9, 5]];
+    var excluded_regions = [[10, 5]];
+
+    let data = [];
+    for(let i = 0; i < targets.length; i++){
+      data.push({data: [targets[i][1]], label: 'Target'});
+    }
+    
+    //let clone = JSON.parse(JSON.stringify(this.barChartData));
+    //console.log(clone);
+
+    //clone = data;
+    //this.barChartData.length = 0;
+    this.barChartData = [
+      {data: [65], label: 'Excluded'},
+      {data: [28], label: 'Target'},
+      {data: [50], label: 'Normal'},
+      {data: [17], label: 'Excluded'}
+    ];
+    //console.log(this.barChartData)
+
+    this.chart.ngOnChanges({} as SimpleChanges);
+
+    //clone = JSON.parse(JSON.stringify(this.chartColors));
+    //clone[0].data = data;
+    //this.chartColors = clone;
   }
 }
