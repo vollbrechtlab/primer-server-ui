c3b9b6e3946e85ebded84159a7199ed8b61ab341
Takao Shibamoto <takaos@iastate.edu>
2017-10-26 18:01:12 -0500

    working scale for paper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component-dev.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   /* General */
   title = 'Primer Server';
-  version = '2.0.5';  
+  version = '2.0.6';  
 
   constructor(){
    
