f7c0dafd0048b16ae0a9e6d64df91178ad329f02
Takao Shibamoto <takaos@iastate.edu>
2017-10-29 18:24:07 -0500

    ver 2.0.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   /* General */
   title = 'Primer Server';
-  version = '2.0.7';  
+  version = '2.0.8';  
 
   constructor(){
    
