8df346cd2f8103f3e856fc5151d7b1e47a75ec2e
Takao <tshibamot42@students.tntech.edu>
2018-03-29 15:11:46 -0500

    small change

---
+++ update_repo.sh
@@ -0,0 +1,23 @@
+# make sure to push dev first
+git checkout dev
+git add --all .
+git commit -m "small change"
+git push origin dev
+
+# copy all dev except .git and venv to /tmp/primer-server-ui-tmp/
+rm -R /tmp/primer-server-ui-tmp
+mkdir /tmp/primer-server-ui-tmp
+rsync -aP . /tmp/primer-server-ui-tmp/ --exclude=.git --exclude=node_modules --exclude=update_repo.sh
+
+# copy back all files in /tmp/primer-server-ui-tmp/ to master
+git checkout master
+mv .git /tmp/primer-server-ui-tmp/
+mv node_modules /tmp/primer-server-ui-tmp/
+rm -fR ./* ./.*
+cp -a /tmp/primer-server-ui-tmp/. ./
+git add --all .
+git commit -m "from dev"
+git push origin master
+
+# come back to dev
+git checkout dev
