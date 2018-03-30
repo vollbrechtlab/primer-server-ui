e650b4276950c6db8326e9dd9aee15b563f29edb
takao42 <takao8@gmx.com>
2018-03-30 09:02:03 -0500

    add constantsService for easy setting

--- src/app/constants/data.service.ts
+++ src/app/constants/data.service.ts
@@ -6,11 +6,25 @@ import { Injectable } from '@angular/core';
 /** Shares all the variables, some basic functions **/
 @Injectable()
 export class ConstantsService {
+  // for basic settings
+  PRIMER_SALT_CORRECTIONS_INPUT_OPTIONS = [
+    {value: 0, viewValue: 'Schildkraut and Lifson 1965'},
+    {value: 1, viewValue: 'SantaLucia 1998'},
+    {value: 2, viewValue: 'Owczarzy et. 2004'}
+  ];
+  PRIMER_TM_FORMULA_INPUT_OPTIONS = [
+    {value: 0, viewValue: 'Breslauer et al. 1986'},
+    {value: 1, viewValue: 'SantaLucia 1998'}
+  ];
+
   // for specificity checking
   GENOME_OPTIONS = [
     {value: 'maize_v3', viewValue: 'Maize B73 AGPv3'},
     {value: 'maize_v4', viewValue: 'Maize V4'}
   ];
+
+  API_URL:string = 'http://localhost:8001/v1.03/';
+
   constructor() {
 
   }
