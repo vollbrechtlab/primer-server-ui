09c7012da7c8b5c028924d28b77bf33202637a84
Takao <tshibamot42@students.tntech.edu>
2018-03-30 13:58:20 -0500

    use environment variables instead

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,26 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// This is a debug settings
 export const environment = {
-  production: false
+  production: false,
+
+    // for basic settings
+  PRIMER_SALT_CORRECTIONS_INPUT_OPTIONS: [
+    {value: 0, viewValue: 'Schildkraut and Lifson 1965'},
+    {value: 1, viewValue: 'SantaLucia 1998'},
+    {value: 2, viewValue: 'Owczarzy et. 2004'}
+  ],
+  PRIMER_TM_FORMULA_INPUT_OPTIONS: [
+    {value: 0, viewValue: 'Breslauer et al. 1986'},
+    {value: 1, viewValue: 'SantaLucia 1998'}
+  ],
+  // for specificity checking
+  GENOME_OPTIONS: [
+    {value: 'maize_v3', viewValue: 'Maize B73 AGPv3'},
+    {value: 'maize_v4', viewValue: 'Maize V4'}
+  ],
+
+  // put the correct API version
+  API_URL: 'http://localhost:8001/v1.03/'
 };
