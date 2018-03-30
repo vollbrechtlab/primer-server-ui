b04c93ac991cca0ef18625932cf48a0410b12bbc
Takao <tshibamot42@students.tntech.edu>
2018-03-30 15:33:48 -0500

    updated settings

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo building version $1
-ng build --bh /primer-server/v$1/
+ng build --prod --env=prod --bh /primer-server/v$1/
 echo $'RewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule .* index.html [L]' > dist/.htaccess 
 sudo rm -fR /var/www/html/primer-server/v$1
 sudo cp -R dist /var/www/html/primer-server/v$1
