396a6cfedccab9e2267355db8820bf846698f28e
Takao Shibamoto <takao8@gmx.com>
2018-04-02 00:59:12 -0500

    updated repdate_repo.sh

--- update_repo.sh
+++ update_repo.sh
@@ -7,8 +7,7 @@ git push origin dev
 # copy all dev except .git and venv to /tmp/primer-server-ui-tmp/
 rm -fR /tmp/primer-server-ui-tmp
 mkdir /tmp/primer-server-ui-tmp
-rsync -aP . /tmp/primer-server-ui-tmp/ --exclude=.git --exclude=node_modules --exclude=update_repo.sh
-
+rsync -aP . /tmp/primer-server-ui-tmp/ --exclude=.git --exclude=node_modules --exclude=update_repo.sh --exclude=deploy_repo.js
 # copy back all files in /tmp/primer-server-ui-tmp/ to master
 git checkout master
 git pull
