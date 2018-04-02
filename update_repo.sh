aeb6344ff14f9b61323586f64074e9de8f7b7d25
Takao Shibamoto <takao8@gmx.com>
2018-04-02 01:08:57 -0500

    small change

--- update_repo.sh
+++ update_repo.sh
@@ -7,6 +7,6 @@ git push origin dev
 # copy all dev except .git and venv to /tmp/primer-server-ui-tmp/
 rm -fR /tmp/primer-server-ui-tmp
 mkdir /tmp/primer-server-ui-tmp
-cp ./{.angular-cli.json .editorconfig .gitignore README.md deploy.js karma.conf.js package-lock.json package.json protractor.conf.js tsconfig.json tslint.json} /tmp/primer-server-ui-tmp/
+cp {.angular-cli.json, .editorconfig, .gitignore, README.md, deploy.js, karma.conf.js, package-lock.json, package.json, protractor.conf.js, tsconfig.json, tslint.json} /tmp/primer-server-ui-tmp/
 
 
