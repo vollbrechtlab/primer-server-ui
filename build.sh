1ff17c31c855a6ca98b170c5e2303534f2feaa36
Takao Shibamoto <takao8@gmx.com>
2018-04-24 17:47:02 -0500

    small change

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 ng build --prod --env=prod --bh /primer-server/
-echo "RewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule .* index.html [L]" > dist/.htaccess
+echo -e 'RewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule .* index.html [L]' > dist/.htaccess
